de09fc8890f754a3c3bd8a3483e0d57e1db47824
nileshpatel17 <nilesh.nvs@hotmail.com>
2020-01-02 16:54:58 +0530

    done

--- pages/hola.js
+++ pages/hola.js
@@ -2,12 +2,15 @@ import * as React from 'react'
 import Link from 'next/link'
 import Layout from '../components/Layout'
 import { version, name } from '../package.json'
+import '../styles/one.css';
+import '../styles/two.scss';
 
 const Hola = () => {
   return (
     <Layout title="Hola | Next.js + TypeScript Example">
       <h1>Name: {name} 👋</h1>
-      <h2>Version: {version}</h2>
+      <span className='header'>Version: {version}</span>
+      <span className='header-g'>Version: {version}</span>
       <p>
         <Link href="/about">
           <a>About</a>
